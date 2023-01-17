e9258984722222ace4a370772544eb2c8439214e
HediKhemiri3001 <khemirimedhedi@gmail.com>
2023-01-17 23:00:32 +0100

    Added Api routes

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -3,24 +3,27 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
-
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
   getAllProducts() {
-    return this.http.get(environment.baseApi +'products')
+    return [
+      this.http.get(environment.baseApi + 'products'),
+      this.http.get(environment.nestApi + 'products'),
+    ];
   }
-
+  getHistoryOfUser(id: number) {
+    return this.http.post(environment.nestApi + 'products/user', { id: id });
+  }
   getAllCategories() {
-    return this.http.get(environment.baseApi +'products/categories')
+    return this.http.get(environment.baseApi + 'products/categories');
   }
-  getProductsByCategory(keyword:string) {
-    return this.http.get(environment.baseApi +'products/category/'+keyword)
+  getProductsByCategory(keyword: string) {
+    return this.http.get(environment.baseApi + 'products/category/' + keyword);
   }
 
-  getProductById(id:any) {
-    return this.http.get(environment.baseApi +'products/'+id)
+  getProductById(id: any) {
+    return this.http.get(environment.baseApi + 'products/' + id);
   }
-  
 }
